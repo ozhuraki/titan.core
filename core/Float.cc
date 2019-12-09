bb776d7b7348e8c7a24e2133d05c269ecae9e2ef
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-12-09 15:21:44 +0100

    minor simplification.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- core/Float.cc
+++ core/Float.cc
@@ -791,12 +791,10 @@ int FLOAT::RAW_decode(const TTCN_Typedescriptor_t& p_td, TTCN_Buffer& buff,
   double tmp = 0.0;
   unsigned char data[16];
   RAW_coding_par cp;
-  boolean orders = FALSE;
-  if (p_td.raw->bitorderinoctet == ORDER_MSB) orders = TRUE;
+  boolean orders = p_td.raw->bitorderinoctet == ORDER_MSB;
   if (p_td.raw->bitorderinfield == ORDER_MSB) orders = !orders;
   cp.bitorder = orders ? ORDER_MSB : ORDER_LSB;
-  orders = FALSE;
-  if (p_td.raw->byteorder       == ORDER_MSB) orders = TRUE;
+  orders = p_td.raw->byteorder == ORDER_MSB;
   if (p_td.raw->bitorderinfield == ORDER_MSB) orders = !orders;
   cp.byteorder = orders ? ORDER_MSB : ORDER_LSB;
   cp.fieldorder = p_td.raw->fieldorder;
